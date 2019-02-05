94fc65fa8411234ece808575d0d1b789a83b4bc1
djfill <philevans02@gmail.com>
2019-02-05 10:49:02 +0000

    Randomised assets

--- create.sh
+++ create.sh
@@ -4,7 +4,15 @@ for i in {1..10}
 do
   :
   mkdir ~/baby-shark-test/assets/${i}
-  touch ~/baby-shark-test/assets/${i}/shark.jpg
 done
-
+touch ~/baby-shark-test/assets/1/babyshark.jpg
+touch ~/baby-shark-test/assets/2/mummyshark.jpg
+touch ~/baby-shark-test/assets/3/daddyshark.jpg
+touch ~/baby-shark-test/assets/4/sistershark.jpg
+touch ~/baby-shark-test/assets/5/brothershark.jpg
+touch ~/baby-shark-test/assets/6/cousinshark.jpg
+touch ~/baby-shark-test/assets/7/happyshark.jpg
+touch ~/baby-shark-test/assets/8/angryshark.jpg
+touch ~/baby-shark-test/assets/9/hungryshark.jpg
+touch ~/baby-shark-test/assets/10/slippyshark.jpg
 
