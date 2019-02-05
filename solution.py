18f7d81957296c630cf275027a7446dea7129bc9
djfill <philevans02@gmail.com>
2019-02-05 09:42:33 +0000

    Added the python solution for the exercise

--- solution.py
+++ solution.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
-
-readlines() 
+import os
+import csv
+dir = os.path.expanduser('~/baby-shark-test/assets/')
+with open('baby-sharks.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        jpg = row[2]
+        no  = row[0]
+        src = dir + no + '/' + os.listdir(dir + no + '/')[0]
+        dest = dir + no + '/' + jpg
+        os.rename(src, dest)
